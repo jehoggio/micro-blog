a4908ee33dbb197ca1fed912e12ea5733b205d43
jeffhogg86 <jeffhogg86@gmail.com>
2021-07-31 22:33:39 -0700

    init commit

---
+++ event-bus/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.post('/events', (req, res) => {
+  const event = req.body
+
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log(err.message)
+  })
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log(err.message)
+  })
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err.message)
+  })
+
+  res.sendStatus(200)
+})
+
+app.listen(4005, () => {
+  console.log(`Event bus listening on port 4005`)
+})
