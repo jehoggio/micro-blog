b82e4355ab644f18071d911fbea6fd53c06ddff4
jeffhogg86 <jeffhogg86@gmail.com>
2021-08-13 10:48:19 -0700

    adds dockerfiles

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -8,13 +9,7 @@ app.use(cors())
 
 const posts = {}
 
-app.get('/posts', (req, res) => {
-  res.send(posts)
-})
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data
 
@@ -26,19 +21,42 @@ app.post('/events', (req, res) => {
   }
 
   if (type === 'CommentCreated') {
-    const { id, content, postId } = data
+    const { id, content, postId, status } = data
     const post = posts[postId]
     post.comments.push({
       id,
       content,
+      status,
     })
   }
 
-  console.log(posts)
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data
+    const post = posts[postId]
+    const comment = post.comments.find((c) => c.id === id)
+    comment.status = status
+    comment.content = content
+  }
+}
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body
+
+  handleEvent(type, data)
 
   res.send({})
 })
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on port 4002')
+
+  const res = await axios.get(`http://localhost:4005/events`)
+  res.data.forEach((event) => {
+    console.log(event)
+    handleEvent(event.type, event.data)
+  })
 })
