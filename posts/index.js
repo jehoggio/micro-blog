38f0807d62f87cab6d70036f646c0969c85f4e29
jeffhogg86 <jeffhogg86@gmail.com>
2021-08-15 16:13:01 -0700

    adds docker image and kubernetes config files

--- posts/index.js
+++ posts/index.js
@@ -19,7 +19,7 @@ app.post('/posts', async (req, res) => {
   const { title } = req.body
   posts[id] = { id, title }
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'PostCreated',
     data: { id, title },
   })
@@ -34,5 +34,6 @@ app.post('/events', (req, res) => {
 })
 
 app.listen(4000, () => {
+  console.log('v2')
   console.log(`Listening on port 4000!`)
 })
